98728ed446f09cd22eaa266afeb9d5e146399e94
Akraja712 <ashokkumar7hitter@gmail.com>
2024-07-19 12:28:19 +0530

    header.php

--- header.php
+++ header.php
@@ -210,8 +210,6 @@ $fn = new custom_functions;
                         <i class="fa fa-angle-right pull-right"></i>
                     </a>
                     <ul class="treeview-menu">
-                        <li><a href="daily.php"><i class="fa fa-angle-right"></i>Daily</a></li>
-                        <li><a href="weekly.php"><i class="fa fa-angle-right"></i>Weekly</a></li>
                         <li><a href="monthly.php"><i class="fa fa-angle-right"></i>Monthly</a></li>
                         <li><a href="yearly.php"><i class="fa fa-angle-right"></i>Yearly</a></li>
                     </ul>
